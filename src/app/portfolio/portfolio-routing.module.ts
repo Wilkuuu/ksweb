bffcbb55a0c71caeb35afc61b920bf67c14bc526
Arkadiusz Szumacher <a.szumacher@boosthigh.com>
2021-02-13 10:42:35 +0100

    dodanie animacji przejścia

--- src/app/portfolio/portfolio-routing.module.ts
+++ src/app/portfolio/portfolio-routing.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PortfolioComponent } from './portfolio.component';
+import { Bh1Component } from './bh1/bh1.component';
+import { GrafitComponent } from './grafit/grafit.component';
 
 const routes: Routes = [
-  {path: '', component: PortfolioComponent}
+    {path: '', redirectTo: 'grafit', pathMatch: 'full'},
+    {path: 'bh1', component: Bh1Component},
+    {
+        path: 'grafit', component: GrafitComponent
+    },
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
 })
-export class PortfolioRoutingModule { }
+export class PortfolioRoutingModule {
+}
