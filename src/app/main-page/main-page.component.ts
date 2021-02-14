18d19d0d11e3c6bc6f3d3cb16c5ede0f5cc18e12
Arkadiusz Szumacher <a.szumacher@boosthigh.com>
2021-02-14 09:06:58 +0100

    Poprawa nazewnictwa plików, dodanie kolejnych stron

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,45 +1,56 @@
 import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
-  selector: 'app-main-page',
-  templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+    selector: 'app-main-page',
+    templateUrl: './main-page.component.html',
+    styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent implements OnInit {
-  @ViewChild('fullpageRef') fp_directive: ElementRef;
+    @ViewChild('fullpageRef') fp_directive: ElementRef;
 
-  config;
-  fullpage_api;
-  pages = ['dashboard', 'grafit', 'bh1', 'bh2', 'swist', 'skrybe', 'gaia-tree', 'galeria', 'o-mnie']
-  constructor(private renderer: Renderer2) {
+    config;
+    fullpage_api;
+    pages = ['dashboard', 'grafit', 'bh1', 'bh2', 'swist', 'skrybe', 'gaia-tree', 'galeria', 'o-mnie'];
 
-    this.config = {
-      licenseKey: '',
-      menu: '#menu',
-      sectionsColor: [],
-      anchors: this.pages,
-      navigation: true,
-      // events callback
-      afterLoad: (origin, destination, direction) => {
-        // console.log(destination);
-      },
-      afterRender: () => {
-        // console.log('afterRender');
-      },
-      afterResize: (width, height) => {
-        // console.log('afterResize' + width + ' ' + height);
-      },
-      afterSlideLoad: (section, origin, destination, direction) => {
-        // console.log(destination);
-      }
-    };
-  }
+    constructor(private renderer: Renderer2) {
 
-  ngOnInit() {
-  }
+        this.config = {
+            licenseKey: '',
+            menu: '#menu',
+            sectionsColor: [],
+            anchors: this.pages,
+            navigation: true,
+            // events callback
+            afterLoad: (origin, destination, direction) => {
+            },
+            onLeave: (section, origin, destination, direction) => {
+                if (origin.anchor === 'o-mnie' || origin.anchor === 'bh1' ) {
+                    this.setColorNavbar('#fff');
+                } else {
+                    this.setColorNavbar('#333');
+                }
+            },
+            afterRender: () => {
+                // console.log('afterRender');
+            },
+            afterResize: (width, height) => {
+                // console.log('afterResize' + width + ' ' + height);
+            },
+            afterSlideLoad: (section, origin, destination, direction) => {
+                // console.log(destination);
+            }
+        };
+    }
 
-  getRef(fullPageRef) {
-    this.fullpage_api = fullPageRef;
-  }
+    ngOnInit() {
+    }
+
+    getRef(fullPageRef) {
+        this.fullpage_api = fullPageRef;
+    }
+
+    setColorNavbar(color: string) {
+        document.documentElement.style.setProperty('--navbar-color', color);
+    }
 
 }
